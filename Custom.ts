ebff6d41356aa618c7e9bf9dda4e3f912debde93
Thawin Chalermdit <42092608+mameeewin@users.noreply.github.com>
2020-06-02 10:09:42 +0700

    Update main.blocks, Custom.ts

--- Custom.ts
+++ Custom.ts
@@ -1,5 +1,5 @@
-//% color=#4b6584 weight=31 icon="\uf03e"
-namespace screenMagic {
+//% color=#007A4B weight=31 icon="\uf03e"
+namespace animation {
     /**
      * Address LEDs linearly row first
      */
@@ -11,6 +11,29 @@ namespace screenMagic {
         const y = Math.floor(index / 5);
         const x = Math.floor(index % 5);
         led.plot(x, y)
-    }      
+        let sprites: game.LedSprite[] = []
+    }
+
+let sprites: game.LedSprite[] = []
+    /**
+     * Blink an LED at a particular interval (ms)
+     * @param x horizontal led coordinate
+     * @param y vertical led coordinate
+     * @param interval time in milli seconds between blinks
+     */
+    //% block="blink x $x y $y every $interval ms"
+    //% x.min=0 x.max=4 y.min=0 y.max=4
+    //% x.min=0 x.max=4 y.min=0 y.max=4
+    //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+    //% interval.shadow=timePicker
+    export function blink(x:number, y: number, interval: number): void {
+        // find an existing sprite
+        let sprite = sprites.find(s => s.x() == x && s.y() == y);
+        if (!sprite)
+            sprite = game.createSprite(x, y)
+        sprite.setBlink(interval)
+        sprite.blink()
+    }   
 }
 
+    
